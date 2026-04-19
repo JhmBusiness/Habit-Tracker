431cfd60d592b48135ccaf7ec079ee36cd4d6685
JhmBusiness <jhm.business@hotmail.com>
2026-04-19 19:57:07 +0100

    Second attempt at fixing login issue with netlify.

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,22 +1,44 @@
+// import { createClient } from "@/app/_lib/supabase/server";
+// import { NextResponse } from "next/server";
+
+// export async function GET(request: Request) {
+//   const requestUrl = new URL(request.url);
+//   const code = requestUrl.searchParams.get("code");
+//   const next = requestUrl.searchParams.get("next") || "/app/dashboard";
+
+//   if (code) {
+//     const supabase = await createClient();
+//     const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+//     if (error) {
+//       console.error("Error exchanging code for session:", error.message);
+//       return NextResponse.redirect(
+//         `${requestUrl.origin}/login?error=${encodeURIComponent(error.message)}`
+//       );
+//     }
+//   }
+
+//   return NextResponse.redirect(`${requestUrl.origin}${next}`);
+// }
+
 import { createClient } from "@/app/_lib/supabase/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
-  const next = requestUrl.searchParams.get("next") || "/app/dashboard";
+
+  const supabase = await createClient();
 
   if (code) {
-    const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (error) {
       console.error("Error exchanging code for session:", error.message);
-      return NextResponse.redirect(
-        `${requestUrl.origin}/login?error=${encodeURIComponent(error.message)}`
-      );
+
+      return NextResponse.redirect(`${requestUrl.origin}/login`);
     }
   }
 
-  return NextResponse.redirect(`${requestUrl.origin}${next}`);
+  return NextResponse.redirect(`${requestUrl.origin}/app/dashboard`);
 }
